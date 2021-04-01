65db13459d6c2463b0d478c6a4881743c687f9eb
Jack Hubert <jphubert93@gmail.com>
2021-04-01 12:34:21 -0600

    fixed download image comment

--- src/abstractor_app/views.py
+++ src/abstractor_app/views.py
@@ -6,7 +6,8 @@ from .models import User, Document, Abstraction
 from .serializers import UserSerializer, DocumentSerializer, AbstractionSerializer
 from django.views import View
 from django.http import HttpResponse
-from script.execute import execute_web_program
+from script.environment import execute_web_program
+from time import sleep
 
 # Create your views here.
 class UserView(viewsets.ModelViewSet):
@@ -28,15 +29,21 @@ class AbstractionView(viewsets.ModelViewSet):
 class ExecuteView(GenericAPIView):
     queryset = Abstraction.objects.all()
     serializer_class = AbstractionSerializer
-    permission_classes = [permissions.AllowAny,]
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request):
         print(request.data["abstraction"])
+        county = request.data["abstraction"]["countyInformation"]
         client = request.data["abstraction"]["clientInformation"]
         legal = request.data["abstraction"]["legalDescription"]
         upload_file = request.data["abstraction"]["uploadFile"].split('\\')[-1]
-        execute_web_program(client, legal, upload_file)
-        return Response(data="some string", status=status.HTTP_200_OK)
+        # print(request.data["abstraction"]["uploadFile"])
+        # print(county)
+        # print(client)
+        # print(legal)
+        # print(upload_file)
+        dictionary_object = execute_web_program(county, client, legal, upload_file[:-5])
+        return Response(data=dictionary_object, status=status.HTTP_200_OK)
 
 
 class ProfileView(viewsets.ViewSet):
